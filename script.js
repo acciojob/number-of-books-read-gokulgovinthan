464dc390b9a9e1007c17746f805e42a30055e76f
gokulgovinthan <goodgovinthan@gmail.com>
2024-01-29 19:37:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
-
-alert(numberOfBooksRead());
+    {
+      author: "Bill Gates",
+      title: "The Road Ahead",
+      readingStatus: true,
+    },
+    {
+      author: "Steve Jobs",
+      title: "Walter Isaacson",
+      readingStatus: true,
+    },
+    {
+      author: "Suzanne Collins",
+      title: "Mockingjay: The Final Book of The Hunger Games",
+      readingStatus: false,
+    },
+  ];
+  
+  const numberOfBooksRead = () => {
+    let a=0;
+     library.forEach(function(library){
+        if(library.readingStatus){
+            a++;
+        }
+     })
+     return a
+  };
+  
+  // Do not change the code below
+  
+  alert(numberOfBooksRead());
+  
